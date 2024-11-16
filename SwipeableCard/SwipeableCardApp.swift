7017f769d600d1235f7c9dad6e2f292410698ee8
khoanisdev <doanvankhoan124@gmail.com>
2024-11-16 23:34:28 +0700

    Initial Commit

---
+++ SwipeableCard/SwipeableCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeableCardApp.swift
+//  SwipeableCard
+//
+//  Created by Đoàn Văn Khoan on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeableCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
